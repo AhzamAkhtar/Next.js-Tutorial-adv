0db91d70c6a2f220e0a38962a8f91dd8d3e820bc
Ahzam Akhtar <ahzam8186@gmail.com>
2022-08-17 00:21:14 +0530

    server side and static functions added

--- pages/Blogs.js
+++ pages/Blogs.js
@@ -1,21 +1,21 @@
+import * as fs from 'fs'
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
-const Blogs = () => {
-  const [blogs,setblogs] = useState([])
-  useEffect(()=>{
+const Blogs = (props) => {
+  const [blogs,setblogs] = useState(props.allBlogs)
+  //console.log(props)
+  // Old Way , Now Doing With getServerSideProps  
+  /*useEffect(()=>{
     console.log("Running")
     fetch("http://localhost:3000/api/fetchallblog").then((a)=>{
       return a.json()
     }).then((data)=>{
-      console.log(data)
+      //console.log(data)
       setblogs(data)
     })
-  },[])
+  },[])*/
 // Step 1 : Collect all the files from blogdata dorectory
 // Step 2 : Iterate through the and display them
-
-  
-
   return (
     <div>
     <h1>POPULAR BLOGS</h1>
@@ -35,6 +35,28 @@ const Blogs = () => {
       </div>
     </div>
   )
+}
+
+/*export async function getServerSideProps(context){
+  let data = await fetch("http://localhost:3000/api/fetchallblog")
+  let allBlogs = await data.json()
+  return{
+    props:{allBlogs}
+  }
+}*/
+
+export async function getStaticProps(context){
+  let data = await fs.promises.readdir("comments")
+  let myfile
+  let allBlogs = []
+  for(let index=0 ;index<data.length;index++){
+    const item = data[index]
+    myfile = await fs.promises.readFile(('comments/'+item),'utf-8')
+    allBlogs.push(JSON.parse(myfile))
+  }
+  return{
+    props:{allBlogs}
+  }
 }
 
 export default Blogs
--- pages/bolgpost/[slags].js
+++ pages/bolgpost/[slags].js
@@ -1,10 +1,14 @@
+import * as fs from 'fs'
 import React, { useEffect, useState } from 'react'
 import {useRouter} from "next/router"
-const slags = () => {
+const Slags = (props) => {
+  //console.log(props)
+  //console.log(props)
   // Step 1 : Find The file corresponding to the slug 
   // Step 2 : populate Them inside the page
-  const [blog,setblog] = useState()
-    const router = useRouter()
+  const [blog,setblog] = useState(props.myBlog)
+  // Now Doinmg With getServerSideProps
+    /*const router = useRouter()
     const slug = router.query.slags
     console.log(slug)
     useEffect(()=>{
@@ -16,7 +20,7 @@ const slags = () => {
         console.log(data)
         setblog(data)
       })
-    },[router.isReady])
+    },[router.isReady])*/
   return (
     <div>
       <h1>{blog && blog.title}</h1>
@@ -29,5 +33,36 @@ const slags = () => {
     </div>
   )
 }
+/*export async function getServerSideProps(context){
+  console.log(context.query.slags)
+  const slug = context.query.slags
+  let data = await fetch(`http://localhost:3000/api/blogss?slug=${slug}`)
+  let myBlog = await data.json()
+  return{
+    props:{myBlog}
+  }
+}*/
 
-export default slags
+export async function getStaticPaths(){
+  return{
+    paths:[
+      {params:{slags:"comments"}},
+      {params:{slags:"data"}},
+      {params:{slags:"data2"}},
+      {params:{slags:"data3"}},
+      {params:{slags:"newdata"}},
+      {params:{slags:"newdata2"}}
+    ],
+    fallback:true
+  }
+}
+
+export async function getStaticProps(context){
+  const {slags} = context.params
+  let myBlog = await fs.promises.readFile(`comments/${slags}.json`,'utf-8')
+  return{
+    props:{myBlog:JSON.parse(myBlog)},
+  }
+}
+
+export default Slags
